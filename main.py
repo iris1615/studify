7957aab82d061e19b7eef88e938ef57be014072b
iris1615 <irisescoval1615@gmail.com>
2025-07-02 13:06:43 +0100

    OAuth setup concluído !!
    Permite a autenticação de utilizadores através do acesso ao access_token- assim como o seu refresh no caso de expiração.
    De momento apenas dá retrieve das playlists do utilizador em formato json,, vou tentar tornar a apresentação mais bonita

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import flask, redirect
 import requests
 import urllib.parse
+import os
+
+from flask import Flask, redirect, jsonify, session, request
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.secret_key = 'random aah string'
@@ -33,3 +36,64 @@ def login():
 
     return redirect(auth_url)
 
+@app.route('/callback') # no caso de recebermos um erro
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    if 'code' in request.args:
+        req_body = {
+            'code': request.args['code'],
+            'grant_type': 'authorization_code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data= req_body) # o que vamos mandar para o spotify
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token'] # para requests
+        session['refresh_token'] = token_info['refresh_token'] # para dar refresh ao access token (apenas dura 1 dia)
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in'] # indica quanto tempo o access_token dura
+        
+        return redirect('/playlists') # vai retornar as playlists do user
+
+@app.route('/playlists')
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    if datetime.now().timestamp() > session['expires_at']:
+        print("TOKEN EXPIRED,, REFRESHING..")
+        return redirect('/refresh-token')
+
+    headers = {
+        'Authorization': f"Bearer {session['access_token']}"
+    }
+    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        print("TOKEN EXPIRED,, REFRESHING..")
+        req_body = {
+            'grant_type': 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        response = requests.post(TOKEN_URL, data=req_body)
+        new_token_info = response.json()
+
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in'] 
+
+        return redirect('/playlists')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug= True)
